0626e0e93b7e9746eb3636f232a68ae9cdd9560c
hardycellreal <hardyzeni547@gmail.com>
2026-04-14 23:19:24 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,25 @@
+// ==========================================
+// KONFIGURASI DATABASE & SETTING
+// ==========================================
+
+// 👇 Masukkan LINK RAW file database.json dari Github kamu di sini
+const DATABASE_URL = "https://raw.githubusercontent.com/hardycellreal/TopUpMurah/main/database.json"; 
+
+// EMAIL ADMIN (JANGAN DIUBAH)
+const ADMIN_EMAIL = "rstu4013@gmail.com";
+
+// BIAYA ADMIN
+const FEE_ADMIN = 1500;
+
+// POTONGAN HARGA PAKAI PROMO
+const DISCOUNT_PRICE = 500;
+
+// REKENING PEMBAYARAN
+const PAYMENT = {
+    method: "DANA",
+    number: "083178509560",
+    name: "akhyaren"
+};
+
+// EMAIL TUJUAN NOTIFIKASI
+const ADMIN_EMAIL_TARGET = "rstu4013@gmail.com";
